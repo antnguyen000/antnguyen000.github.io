77e9d24a4087d25fe9aae28a3e26d4d682f33fd3
Anthony Nguyen <antnguyen000@gmail.com>
2023-01-09 00:14:54 -0600

    Add information to the about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,14 @@
+import portrait from "../images/portrait.JPG"
+
 const About = () => {
     return (
-        <div className="about"></div>
+        <div className="about">
+            <img src={ portrait } alt="portrait"></img>
+            <div className="about-text">
+                <h2>Hello! My name is Anthony Nguyen!</h2>
+                <p>I am a 3rd year mechanical engineering student at the University of Texas at Austin with building passion in software engineering! I am pursuing the UT Austin Elements of Computing certificate to expand my knowledge of programming and I hope to be able to develop software in the future!</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import About from "./About"
 const Home = () => {
     return (
         <div className="home">
+            <h1 className="title">Anthony's Portfolio</h1>
             <About></About>
         </div>
     )
