39bc22e054029129986925adb3ef5e4489f028e0
Anthony Nguyen <antnguyen000@gmail.com>
2023-01-18 00:04:59 -0600

    Add target blank to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="https://github.com/antnguyen000"><BsGithub size={ 50 }/></a>
-            <a href="https://linkedin.com/in/anthony-nguyen-b0b15a232"><BsLinkedin size={ 50 }/></a>
+            <a href="https://github.com/antnguyen000" target="_blank" rel="noreferrer"><BsGithub size={ 50 }/></a>
+            <a href="https://linkedin.com/in/anthony-nguyen-b0b15a232" target="_blank" rel="noreferrer"><BsLinkedin size={ 50 }/></a>
         </div>
     )
 }
