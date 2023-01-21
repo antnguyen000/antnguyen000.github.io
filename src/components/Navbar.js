04507702d727f61d99a0c09fc58b66c208b6a5e4
Anthony Nguyen <antnguyen000@gmail.com>
2023-01-20 20:56:00 -0600

    Removing resources in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ const Navbar = () => {
                 <ul>
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
-                    <li><a href="resources">Resources</a></li>
                     <li><a href="https://github.com/antnguyen000" target="_blank" rel="noreferrer">Github</a></li>
                 </ul>
             </div>
